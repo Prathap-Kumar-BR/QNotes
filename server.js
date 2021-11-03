75ea28621267f67ee900e8fdddfc64859445b051
prathapbr1993 <82834371+prathapbr1993@users.noreply.github.com>
2021-11-03 12:26:29 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./server/config/db.js";
+import colors from "colors";
+import path from "path";
+import noteRoutes from "./server/routes/noteRoutes.js";
+import userRoutes from "./server/routes/userRoutes.js";
+import { errorHandler, notFound } from "./server/middleware/errorMiddleware.js";
+
+dotenv.config();
+connectDB();
+
+const app = express(); // main thing
+
+app.use(express.json()); // to accept json data
+
+app.use("/api/notes", noteRoutes);
+app.use("/api/users", userRoutes);
+
+// --------------------------deployment------------------------------
+
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+
+  app.get("/*", (req, res) =>
+    res.sendFile(path.join(__dirname, "/client/build/index.html"))
+  );
+}
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, console.log(`Server running  on port ${PORT}..`.yellow.bold));
