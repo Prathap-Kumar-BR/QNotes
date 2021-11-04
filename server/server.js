459f6d22c5e144f093428c35f0d785caab5ab9d4
prathapbr1993 <82834371+prathapbr1993@users.noreply.github.com>
2021-11-04 12:27:33 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./server/config/db.js";
+import connectDB from "./config/db.js";
 import colors from "colors";
 import path from "path";
-import noteRoutes from "./server/routes/noteRoutes.js";
-import userRoutes from "./server/routes/userRoutes.js";
-import { errorHandler, notFound } from "./server/middleware/errorMiddleware.js";
+import noteRoutes from "./routes/noteRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
@@ -18,20 +18,24 @@ app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
 
 // --------------------------deployment------------------------------
+const __dirname = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 // --------------------------deployment------------------------------
 
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
-
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
-
-  app.get("/*", (req, res) =>
-    res.sendFile(path.join(__dirname, "/client/build/index.html"))
-  );
-}
 
 const PORT = process.env.PORT || 4000;
 
