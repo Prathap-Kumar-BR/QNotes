67e2e40f986a625702cc54486c3cd87a77a9e90e
prathapbr1993 <82834371+prathapbr1993@users.noreply.github.com>
2021-11-23 14:10:06 +0530

    first commit

--- index.js
+++ index.js
@@ -18,12 +18,19 @@ app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
 
 // --------------------------deployment------------------------------
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
 
-  app.get("/*", (req, res) =>
-    res.sendFile(path.join(__dirname, "/client/build/index.html"))
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "/client/build/index.html"))
   );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
 }
 // --------------------------deployment------------------------------
 
